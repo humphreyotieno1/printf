3e1a1b2c167f86876260b3cf5888257c11846287
Humphrey <humphreyotieno04@gmail.com>
2023-07-24 11:06:32 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -5,51 +5,47 @@
  * @format: The format string containing conversion specifiers.
  * Return: The number of characters printed (excluding the null byte).
  */
-
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int printed_chars = 0;
+va_list args;
+int printed_chars = 0;
 
-	if (format == NULL)
-		return (-1);
+if (format == NULL)
+return (-1);
+va_start(args, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+printed_chars += _putchar(va_arg(args, int));
+break;
 
-	va_start(args, format);
+case 's':
+printed_chars += _print_str(va_arg(args, char *));
+break;
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-			case 'c':
-				printed_chars += _putchar(va_arg(args, int));
-				break;
-			case 's':
-				printed_chars += _print_str(va_arg(args, char *));
-				break;
-			case '%':
-				printed_chars += _putchar('%');
-				break;
-			default:
-				printed_chars += _putchar('%');
-				printed_chars += _putchar(*format);
-				break;
-			}
-			format++;//Skip the character after '%'
-		}
-		/**
-		else
-		{
-			printed_chars += _putchar(*format);
-		}
+case '%':
+printed_chars += _putchar('%');
+break;
 
-		format++;
-		*/
-	}
+default:
+printed_chars += _putchar('%');
+printed_chars += _putchar(*format);
+break;
+}
 
-	va_end(args);
-
-	return (printed_chars);
+format++;
+}
+else
+{
+printed_chars += _putchar(*format);
+}
+format++;
+}
+va_end(args);
+return (printed_chars);
 }
