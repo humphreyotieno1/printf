b09049fc30b22d839537c41dfd364e6c0235d902
benzoic09 <benzoic09@gmail.com>
2023-07-25 10:33:39 -0700

    updating functions

--- _printf.c
+++ _printf.c
@@ -25,40 +25,57 @@ int _printf(const char *format, ...)
 			if (*format == '\0') /* Invalid conversion: % at the end of format */
 				return (-1);
 
-			switch (*format)
-			{
-			case 'c':
-				printed_chars += _putchar(va_arg(args, int));
-				break;
-			case 's':
-				{
-					char *str = va_arg(args, char *);
-					if (str == NULL)
-						str = "(null)";
-					printed_chars += _print_str(str);
-					break;
-				}
-			case '%':
-				printed_chars += _putchar('%');
-				break;
-			case 'd':
-			case 'i':
-				printed_chars += _print_int(va_arg(args, int));
-				break;
-			default:
-				printed_chars += _putchar('%');
-				printed_chars += _putchar(*format);
-				break;
-			}
+			printed_chars += handle_conversion(format, args);
+			format++;
 		}
 		else
 		{
 			printed_chars += _putchar(*format);
+			format++;
 		}
-		format++;
 	}
 
 	va_end(args);
+
+	return (printed_chars);
+}
+
+/**
+ * handle_conversion - Handles the conversion specifier in the format string.
+ * @format: Pointer to the conversion specifier character.
+ * @args: The va_list of arguments.
+ * Return: The number of characters printed for this conversion.
+ */
+int handle_conversion(const char *format, va_list args)
+{
+	int printed_chars = 0;
+
+	switch (*format)
+	{
+		case 'c':
+			printed_chars += _putchar(va_arg(args, int));
+			break;
+		case 's':
+			{
+				char *str = va_arg(args, char *);
+
+				if (str == NULL)
+					str = "(null)";
+				printed_chars += _print_str(str);
+				break;
+			}
+		case '%':
+			printed_chars += _putchar('%');
+			break;
+		case 'd':
+		case 'i':
+			printed_chars += _print_int(va_arg(args, int));
+			break;
+		default:
+			printed_chars += _putchar('%');
+			printed_chars += _putchar(*format);
+			break;
+	}
 
 	return (printed_chars);
 }
