a566346b3d219ebe0749a8a26d0877f7a7d58c95
Humphrey <humphreyotieno04@gmail.com>
2023-07-27 15:34:40 -0700

    updated printf

--- _printf.c
+++ _printf.c
@@ -9,10 +9,6 @@ int _print_str(char *str);
 int _print_int(int n);
 int _putchar(char c);
 int _print_ptr(void *ptr);
-int _print_uint(unsigned int n);
-int _print_oct(unsigned int n);
-int _print_hex(unsigned int n, int uppercase);
-int _print_str_non_printable(char *str);
 
 /**
  * _printf - Produces output according to a format.
@@ -85,29 +81,6 @@ int handle_conversion(const char *format, va_list args)
 		case 'i':
 			printed_chars += _print_int(va_arg(args, int));
 			break;
-			case 'u':
-			printed_chars += _print_uint(va_arg(args, unsigned int));
-			break;
-			case 'o':
-			printed_chars += _print_oct(va_arg(args, unsigned int));
-			break;
-			case 'x':
-			printed_chars += _print_hex(va_arg(args, unsigned int), 0);
-			break;
-			case 'X':
-			printed_chars += _print_hex(va_arg(args, unsigned int), 1);
-			break;
-			case 'S':
-			{
-				char *str = va_arg(args, char *);
-
-				if (str == NULL)
-					str = "(null)";
-				printed_chars += _print_str_non_printable(str);
-				break;
-			}
-			case 'p':
-			printed_chars += _print_ptr(va_arg(args, void *));
 		case 'b':
 			{
 				unsigned int num = va_arg(args, unsigned int);
@@ -131,25 +104,4 @@ int handle_conversion(const char *format, va_list args)
 	}
 	return (printed_chars);
 }
-/*
- * main - implementations of the functions
- * Return: 0
- */
 
-int main(void)
-{
-	unsigned int num = 42;
-
-	char *non_printable_str = "Hello, \nWorld!\t";
-
-	int *ptr = &num;
-
-	_printf("Unsigned Decimal: %u\n", num);
-	_printf("Octal: %o\n", num);
-	_printf("Lowercase Hex: %x\n", num);
-	_printf("Uppercase Hex: %X\n", num);
-	_printf("Octal String: %S\n", non_printable_str);
-	_printf("Pointer Address: %p\n", ptr);
-	_printf("Binary representation: %b\n", num);
-	return (0);
-}
